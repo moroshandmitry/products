1014541be9ff105c6557ce70fa0e3538beaea91f
Dmitriy <moroshandmitry@gmail.com>
2022-03-05 20:45:46 +0200

    memorizedProductsOnPage

--- components/CatalogProducts/CatalogProducts.jsx
+++ components/CatalogProducts/CatalogProducts.jsx
@@ -1,17 +1,23 @@
-import { useContext } from 'react';
+import { useContext, useMemo } from 'react';
 import { CatalogContext } from '../../Provider';
 import { ProductCard } from '../../components';
 
 export const CatalogProducts = () => {
 	const { products, page, limitOfProductsOnPage } = useContext(CatalogContext);
 
-	return (
-		<div className='flex flex-wrap justify-around py-3'>
-			{products
+	const memorizedProductsOnPage = useMemo(
+		() =>
+			products
 				.map((product) => (
 					<ProductCard product={product} key={product.node.name} />
 				))
-				.splice(page * limitOfProductsOnPage, limitOfProductsOnPage)}
+				.splice(page * limitOfProductsOnPage, limitOfProductsOnPage),
+		[products, limitOfProductsOnPage, page]
+	);
+
+	return (
+		<div className='flex flex-wrap justify-around py-3'>
+			{memorizedProductsOnPage}
 		</div>
 	);
 };
