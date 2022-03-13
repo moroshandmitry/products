36740ba961dccae1d78802461cdbb90721fa338d
moroshandmitry <moroshandmitry@gmail.com>
2022-03-13 12:18:22 +0200

    created first test

--- utils/range.js
+++ utils/range.js
@@ -1,4 +1,11 @@
 export const range = (start, end) => {
-	const length = end - start + 1;
-	return Array.from({ length }, (_, idx) => idx + start);
+	const numStart = Number(start);
+	const numEnd = Number(end);
+	const length = numEnd - numStart + 1;
+
+	if (numStart < 0 || numEnd < 0) {
+		return [];
+	}
+
+	return Array.from({ length }, (_, idx) => idx + numStart);
 };
